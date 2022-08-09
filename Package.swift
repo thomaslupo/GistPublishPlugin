0d84dc1a76dd2f0765a96a2337ad5c033f862c13
Thomas Lupo <lupo.thomas@gmail.com>
2022-08-09 16:56:28 +0200

    Update Publish version and swift-tools-version

--- Package.swift
+++ Package.swift
@@ -1,10 +1,13 @@
-// swift-tools-version:5.2
+// swift-tools-version:5.5
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
 
 let package = Package(
     name: "GistPublishPlugin",
+    platforms: [
+        .macOS(.v12)
+    ],
     products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
@@ -12,7 +15,7 @@ let package = Package(
             targets: ["GistPublishPlugin"]),
     ],
     dependencies: [
-        .package(name: "Publish", url: "https://github.com/johnsundell/publish.git", from: "0.1.0")
+        .package(name: "Publish", url: "https://github.com/johnsundell/publish.git", from: "0.9.0")
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
